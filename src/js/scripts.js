0abff61956d3ba4e907400cfb1d7088a3af94b0c
on-the-dog <onthedog0819@gmail.com>
2022-07-30 16:05:17 +0200

    renew rebase

---
+++ src/js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Full Width Pics v5.0.5 (https://startbootstrap.com/template/full-width-pics)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// Change the second argument to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('video', {captions: {active: true}});
+
+// Expose player so it can be used from the console
+window.player = player;
